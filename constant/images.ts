22d9d8cda2dfd0f0f25bc1ea39ba9658be58c826
Devesh9321 <curiousforlearningtech@gmail.com>
2025-06-04 18:38:22 +0530

    adding auth ui in app

---
+++ constant/images.ts
@@ -0,0 +1,7 @@
+import onboarding1 from '../assets/images/tmp1.png';
+import logo from '../assets/icon.png';
+
+export const images = {
+  onboarding1,
+  logo,
+};
--- constant/images.tsx
+++
@@ -1,7 +0,0 @@
-import onbord1 from "../assets/images/tmp1.png";
-
-const image = {
-    onbord1
-}
-
-export default image;
